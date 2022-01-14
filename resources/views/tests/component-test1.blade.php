e003c3aed2bfe905e8f76c371d08c5c9e43393f6
kennakafuji <bvlsvb189@gmail.com>
2022-01-14 22:36:19 +0900

    conponent準備

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,9 @@
     <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト１
 
-<x-tests.card title="タイトル" content="本文" />
+<x-tests.card title="タイトル" content="本文" :message="$message" />
+<x-tests.card title="タイトル２" />
+<x-tests.card title="CSSを変更したい" class="bg-red-300" />
+
 
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,1 +1,8 @@
+
+<x-tests.app>
+<x-slot name="header">ヘッダー２</x-slot>
+
 コンポーネントテスト2
+<x-test-class-base />
+
+</x-tests.app>
